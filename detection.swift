306345f056135f8ad4d7f8e855fc78d1c88d6105
Yusuke Toyama <131319857+koko1928@users.noreply.github.com>
2023-10-23 20:43:37 +0900

    Update detection.swift
    Enhanced detection logic.

--- detection.swift
+++ detection.swift
@@ -3,26 +3,64 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        if jailbreakCheck() {
+        if isJailbroken() {
             print("Jailbreak detected")
         } else {
             print("Jailbreak not detected")
         }
     }
-    
-    func jailbreakCheck() -> Bool {
-        let fileManager = FileManager.default
-        if fileManager.fileExists(atPath: "/Applications/Cydia.app") ||
-            fileManager.fileExists(atPath: "/Library/MobileSubstrate/MobileSubstrate.dylib") ||
-            fileManager.fileExists(atPath: "/bin/bash") ||
-            fileManager.fileExists(atPath: "/usr/sbin/sshd") ||
-            fileManager.fileExists(atPath: "/etc/apt") ||
-            fileManager.fileExists(atPath: "/private/var/lib/apt/") ||
-            fileManager.fileExists(atPath: "/Applications/blackra1n.app") ||
-            fileManager.fileExists(atPath: "/Applications/RockApp.app") ||
-            fileManager.fileExists(atPath: "/Applications/SBSettings.app") ||
-            fileManager.fileExists(atPath: "/Applications/FakeCarrier.app") ||
-            fileManager.fileExists(atPath: "/Applications/WinterBoard.app") {
+
+    func isJailbroken() -> Bool {
+        if isFileExist("/Applications/Cydia.app") ||
+           isFileExist("/Library/MobileSubstrate/MobileSubstrate.dylib") ||
+           isFileExist("/bin/bash") ||
+           isFileExist("/usr/sbin/sshd") ||
+           isFileExist("/etc/apt") ||
+           isFileExist("/private/var/lib/apt/") ||
+           isFileExist("/Applications/iFile.app") ||
+           isFileExist("/Applications/FakeCarrier.app") ||
+           isFileExist("/Applications/SBSettings.app") {
+            return true
+        }
+
+        if isAppRunning("Cydia") ||
+           isAppRunning("MobileSubstrate") ||
+           isAppRunning("sshd") ||
+           isAppRunning("sbsettings") {
+            return true
+        }
+
+        if isSymbolicLink("/Applications") ||
+           isSymbolicLink("/usr/libexec") {
+            return true
+        }
+
+        if hasEntitlement("com.apple.springboard.debug") {
+            return true
+        }
+
+        return false
+    }
+
+    func isFileExist(_ path: String) -> Bool {
+        return FileManager.default.fileExists(atPath: path)
+    }
+
+    func isAppRunning(_ appName: String) -> Bool {
+        if let task = ProcessInfo.processInfo.runningProcesses.first(where: { $0.lowercased() == appName.lowercased() }) {
+            return true
+        }
+        return false
+    }
+
+    func isSymbolicLink(_ path: String) -> Bool {
+        var isSymbolic: ObjCBool = false
+        let exists = FileManager.default.fileExists(atPath: path, isDirectory: &isSymbolic)
+        return exists && isSymbolic.boolValue
+    }
+
+    func hasEntitlement(_ entitlement: String) -> Bool {
+        if let data = try? Data(contentsOf: URL(fileURLWithPath: "/Library/MobileSubstrate/DynamicLibraries/" + entitlement + ".dylib")) {
             return true
         }
         return false
